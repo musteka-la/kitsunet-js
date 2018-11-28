6d7abe73c31b8539990618a00b0ca001dea4031a
kumavis <aaron@kumavis.me>
2018-11-28 00:24:18 -0500

    remove try-catch wrapper

--- src/kitsunet-factory.js
+++ src/kitsunet-factory.js
@@ -9,44 +9,40 @@ const createNode = require('./kitsunet-peer/libp2p')
 const log = require('debug')('kitsunet:factory')
 
 module.exports = async function ({ options, identity, addrs }) {
-  try {
-    const node = await createNode({ identity, addrs, bootstrap: options.libp2pBootstrap })
-    const providerTools = createEthProvider({
-      node,
-      rpcUrl: options.rpcUrl,
-      depth: options.sliceDepth,
-      rpcEnableTracker: options.rpcEnableTracker
-    })
+  const node = await createNode({ identity, addrs, bootstrap: options.libp2pBootstrap })
+  const providerTools = createEthProvider({
+    node,
+    rpcUrl: options.rpcUrl,
+    depth: options.sliceDepth,
+    rpcEnableTracker: options.rpcEnableTracker
+  })
 
-    const { blockTracker, sliceTracker } = providerTools
+  const { blockTracker, sliceTracker } = providerTools
 
-    let paths = options.slicePath || []
-    let ethAddrs = options.ethAddrs || []
-    if (ethAddrs.length) {
-      paths = paths.concat(ethAddrs.map((a) => {
-        if (ethUtil.isValidAddress(a)) {
-          return ethUtil.keccak256(ethUtil.stripHexPrefix(a)).toString('hex').slice(0, 4)
-        }
-      }))
-    }
-
-    let slices = paths.map((p) => { return { path: String(p), depth: Number(options.sliceDepth) } })
-    if (options.sliceFile && options.sliceFile.length > 0) {
-      const sclicesFile = require(options.sliceFile)
-      slices = slices.concat(sclicesFile.slices.map((p) => { return { path: String(p), depth: Number(options.sliceDepth) } }))
-    }
-
-    const kitsunet = new Kitsunet({
-      node,
-      blockTracker,
-      sliceTracker,
-      bridgeRpc: options.rpcUrl,
-      isBridge: options.sliceBridge,
-      slices
-    })
-
-    return { providerTools, kitsunet, node }
-  } catch (err) {
-    log(err)
+  let paths = options.slicePath || []
+  let ethAddrs = options.ethAddrs || []
+  if (ethAddrs.length) {
+    paths = paths.concat(ethAddrs.map((a) => {
+      if (ethUtil.isValidAddress(a)) {
+        return ethUtil.keccak256(ethUtil.stripHexPrefix(a)).toString('hex').slice(0, 4)
+      }
+    }))
   }
+
+  let slices = paths.map((p) => { return { path: String(p), depth: Number(options.sliceDepth) } })
+  if (options.sliceFile && options.sliceFile.length > 0) {
+    const sclicesFile = require(options.sliceFile)
+    slices = slices.concat(sclicesFile.slices.map((p) => { return { path: String(p), depth: Number(options.sliceDepth) } }))
+  }
+
+  const kitsunet = new Kitsunet({
+    node,
+    blockTracker,
+    sliceTracker,
+    bridgeRpc: options.rpcUrl,
+    isBridge: options.sliceBridge,
+    slices
+  })
+
+  return { providerTools, kitsunet, node }
 }
