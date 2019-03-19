b328026139f595dccbd62696466414c3d71c204a
Dmitriy Ryajov <dryajov@gmail.com>
2019-03-19 01:30:21 -0600

    use arrays for deps

--- src/stores/dependencies.js
+++ src/stores/dependencies.js
@@ -7,7 +7,7 @@ const MemoryStore = require('interface-datastore').MemoryDatastore
 
 module.exports = (container) => {
   container.registerType('data-store', MemoryStore)
-  container.registerFactory('store', (dataStore) => new Store(dataStore), 'data-store')
-  container.registerFactory('kitsunet-store', (dataStore) => new KitsunetStore(dataStore), 'store')
+  container.registerFactory('store', (dataStore) => new Store(dataStore), ['data-store'])
+  container.registerFactory('kitsunet-store', (dataStore) => new KitsunetStore(dataStore), ['store'])
   return container
 }
