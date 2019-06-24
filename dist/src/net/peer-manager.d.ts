343ba80f258e1419b0725e0ff1c8533b2248cf67
Dmitriy Ryajov <dryajov@gmail.com>
2019-06-24 16:44:44 -0600

    wip: reworking downloads

--- dist/src/net/peer-manager.d.ts
+++ dist/src/net/peer-manager.d.ts
@@ -3,9 +3,10 @@ import { EventEmitter as EE } from 'events';
 import { NodeManager } from './node-manager';
 import { ICapability } from './interfaces';
 import { Peer } from './helper-types';
+import LRUCache from 'lru-cache';
 export declare class PeerManager extends EE {
     nodeManager: NodeManager<Peer>;
-    peers: Map<string, Peer>;
+    peers: LRUCache<string, Peer>;
     constructor(nodeManager: NodeManager<Peer>);
     getById(id: string): Peer | undefined;
     getByCapability(cap: ICapability): Peer[];
@@ -13,5 +14,6 @@ export declare class PeerManager extends EE {
     getUnusedPeers(): Peer[];
     getRandomPeer(): Peer | undefined;
     releasePeers(peers: Peer[]): void;
+    reserve(peers: Peer[]): void;
 }
 //# sourceMappingURL=peer-manager.d.ts.map
--- src/downloader/downloaders/fast-sync-downloader.ts
+++ src/downloader/downloaders/fast-sync-downloader.ts
@@ -1,62 +1,103 @@
 'use strict'
 
-import { DownloaderType } from '../interfaces'
+import BN from 'bn.js'
 import Block from 'ethereumjs-block'
+
+import { DownloaderType } from '../interfaces'
 import {
-  EthProtocol,
   BlockBody,
-  IPeerDescriptor
+  Peer,
+  IEthProtocol,
+  PeerManager,
+  EthProtocol
 } from '../../net'
 import { EthChain } from '../../blockchain'
-
-import BN from 'bn.js'
+import { BaseDownloader } from './base'
+import { queue, AsyncQueue } from 'async'
 
 import Debug from 'debug'
-import { BaseDownloader } from './base'
 const debug = Debug('kitsunet:downloaders:fast-sync')
 
 const MAX_PER_REQUEST: number = 128
+const CONCCURENT_REQUESTS: number = 5
 
-export class FastSyncDownloader<T extends IPeerDescriptor<any>> extends BaseDownloader<T> {
-  constructor (public protocol: EthProtocol<T>,
-               public peer: IPeerDescriptor<T>,
-               public chain: EthChain) {
-    super(protocol, DownloaderType.FAST, chain)
+async function task (this: FastSyncDownloader, peer: Peer) {
+  const protocol = peer.protocols.get('eth')
+  if (!protocol) throw new Error('fast sync requires the ETH capability!')
+
+  let blockNumber: BN = new BN(0)
+  const block: Block | undefined = await this.chain.getLatestBlock()
+  if (block) {
+    blockNumber = new BN(block.header.number)
   }
 
-  async download (): Promise<void> {
-    let blockNumber: BN = new BN(0)
-    const block: Block | undefined = await this.chain.getLatestBlock()
-    if (block) {
-      blockNumber = new BN(block.header.number)
-    }
+  try {
+    debug(`trying to sync with ${protocol.peer.id}`)
+    const remoteHeader: Block | undefined = await this.latest(protocol as unknown as IEthProtocol, peer)
+    if (remoteHeader) {
+      const remoteNumber: BN = new BN(remoteHeader.header.number)
+      const blockNumberStr: string = blockNumber.toString(10)
+      debug(`latest block is ${blockNumberStr} remote block is ${remoteNumber.toString(10)}`)
 
-    try {
-      debug(`trying to sync with ${this.protocol.peer.id}`)
-      const remoteHeader: Block | undefined = await this.latest()
-      if (remoteHeader) {
-        const remoteNumber: BN = new BN(remoteHeader.header.number)
-        const blockNumberStr: string = blockNumber.toString(10)
-        debug(`latest block is ${blockNumberStr} remote block is ${remoteNumber.toString(10)}`)
-
-        // increment current block to set as start
-        blockNumber.iaddn(1)
-        while (blockNumber.lte(remoteNumber)) {
-          debug(`requesting ${MAX_PER_REQUEST} blocks from ${this.protocol.peer.id} starting ` +
+      // increment current block to set as start
+      blockNumber.iaddn(1)
+      while (blockNumber.lte(remoteNumber)) {
+        debug(`requesting ${MAX_PER_REQUEST} blocks from ${protocol.peer.id} starting ` +
           `from ${blockNumberStr}`)
 
-          let headers: Block.Header[] = await this.getHeaders(blockNumber, MAX_PER_REQUEST)
-          if (!headers.length) return
+        let headers: Block.Header[] = await this.getHeaders(protocol as unknown as IEthProtocol,
+          blockNumber, MAX_PER_REQUEST)
 
-          let bodies: BlockBody[] = await this.getBodies(headers.map(h => h.hash()))
-          await this.chain.putBlocks(bodies.map((body, i) => new Block([headers[i].raw].concat(body))))
+        if (!headers.length) return
 
-          blockNumber.iaddn(headers.length)
-          debug(`imported ${headers.length} blocks`)
-        }
+        let bodies: BlockBody[] = await this.getBodies(protocol as unknown as IEthProtocol,
+          headers.map(h => h.hash()))
+
+        await this.store(bodies.map((body, i) => new Block([headers[i].raw].concat(body))))
+
+        blockNumber.iaddn(headers.length)
+        debug(`imported ${headers.length} blocks`)
       }
-    } catch (err) {
-      debug(err)
     }
+  } catch (err) {
+    debug(err)
+  }
+}
+
+export class FastSyncDownloader extends BaseDownloader {
+  type: DownloaderType
+  queue: AsyncQueue<Peer>
+
+  constructor (public chain: EthChain, peerManager: PeerManager) {
+    super(chain, peerManager)
+    this.type = DownloaderType.FAST
+    this.queue = queue(task.bind(this), CONCCURENT_REQUESTS)
+  }
+
+  async best (): Promise<Peer | undefined> {
+    const peers: Peer[] = this.peerManager.getByCapability({
+      id: 'eth',
+      versions: ['63']
+    })
+
+    if (!peers.length) return
+    const status = await Promise.all(peers.map((p) =>
+        (p.protocols.get('eth') as EthProtocol<any>)!.getStatus()))
+
+    let bestPeer: Peer | undefined
+    let bestPeerTd: BN = await this.chain.getBlocksTD()
+    status.forEach((s, i) => {
+      if (s.td.gt(bestPeerTd)) bestPeer = peers[i]
+    })
+
+    if (bestPeer) {
+      this.peerManager.reserve([bestPeer])
+    }
+
+    return bestPeer
+  }
+
+  async download (peer: Peer): Promise<void> {
+    return this.queue.push(peer)
   }
 }
